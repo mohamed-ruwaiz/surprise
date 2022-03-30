58f8d2b2bec5b6f8988b8b72a028686725dc39e8
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-30 14:10:00 +0530

    Add files via upload

---
+++ FiboSurp.java
@@ -0,0 +1,32 @@
+package myjava1;
+
+public class FiboSurp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				int n=10;
+				int n1=2,n2=3,n3;
+				System.out.print(n1+" "+n2+" ");
+				int i=2;
+				while(i<n) {
+					n3=n1+n2;
+					int m=n3/2;
+					int flag=0;
+					for(int j=2;j<=m;j++) {
+						if(n3%j==0) {
+							//System.out.print(n3+" ");
+							flag=1;
+							break;
+						}
+					}
+					if(flag==0) {
+						System.out.print(n3+" ");
+						i++;
+					}
+					n1=n2;
+					n2=n3;	
+				}	
+			}
+		}
+	
